ea57b0272183e3198645172be2af5af14ae710a2
LuckDuracell <lukedru@yahoo.com>
2021-06-11 00:58:44 -0700

    Xcode 13 makes adding Done buttons to keyboards a lot easier. Here's a quick project to show how that uses only one state variable for TextFields. No more Introspect! :D

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,15 +2,61 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by Luke Drushell on 6/11/21.
+//  Created by Luke Drushell on 6/10/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState var showKeyboard: Bool
+    @State var input = ""
+    @State var input2 = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                TextField("Type here", text: $input)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                    .focused($showKeyboard)
+                    .padding()
+                
+                HStack {
+                    Text("Input 1: \(input)")
+                        .font(.title)
+                        .padding()
+                    Spacer()
+                }
+                
+                TextField("Type here", text: $input2)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                    .focused($showKeyboard)
+                    .padding()
+                
+                HStack {
+                    Text("Input 2: \(input2)")
+                        .font(.title)
+                        .padding()
+                    Spacer()
+                }
+                
+            }
+            .navigationTitle("Done Button Test")
+            .toolbar(content: {
+                ToolbarItem(placement: .keyboard, content: {
+                    HStack() {
+                        Spacer()
+                        Button {
+                            showKeyboard.toggle()
+                        } label: {
+                            Text("Done")
+                                .multilineTextAlignment(.trailing)
+                        }
+                    }
+                })
+            })
+        }
     }
 }
 
@@ -19,3 +65,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
